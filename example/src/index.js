a49a837cb37eb11c895294d22c5c65f5379e912c
afeiship <1290657123@qq.com>
2021-11-19 09:48:31 +0800

    feat: add mind/example

---
+++ example/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+
+const root = document.getElementById('root')
+
+// reducer 纯函数
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+// 创建一个store
+const store = createStore(reducer)
+
+const render = () => ReactDOM.render(
+  <div>
+    <span>{store.getState()}</span>
+    <button onClick={() => store.dispatch({type: 'INCREMENT'})}>INCREMENT</button>
+    <button onClick={() => store.dispatch({type: 'DECREMENT'})}>DECREMENT</button>
+  </div>,
+  root
+)
+render()
+// store订阅一个更新函数，待dispatch之后，执行这个更新函数，获取新的值
+store.subscribe(render)
